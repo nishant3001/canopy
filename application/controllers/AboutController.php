ddf7b688c5fbfd2088f372a7ccdfd69402626969
nishantcbse <nishant_cbse@yahoo.com>
2012-11-10 03:44:51 +0530

    first

---
+++ application/controllers/AboutController.php
@@ -0,0 +1,34 @@
+<?php
+class AboutController extends Zend_Controller_Action {
+	
+	public function init(){
+		
+		
+	}
+	
+	public function indexAction(){
+		
+
+    }
+		public function coreAction(){
+		
+
+    }
+			public function overviewAction(){
+		
+
+    }
+			public function socialAction(){
+		
+
+    }
+			public function newsAction(){
+		
+
+    }
+
+
+	
+
+}
+?>
